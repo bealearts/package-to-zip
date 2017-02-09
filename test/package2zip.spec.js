619f730556d1029cd723d8ed9d8c07cde7ab51fd
davidbeale <david.beale@bealearts.co.uk>
2017-02-09 08:36:30 +0000

    Test zip file contents

--- test/package2zip.spec.js
+++ test/package2zip.spec.js
@@ -1,19 +1,50 @@
 
 import package2zip from '../';
 import fs from 'fs';
+import temp from 'temp';
+import { expect } from 'chai';
+import DecompressZip from 'decompress-zip';
+
+temp.track();
 
 describe('package2zip', () => {
     
     it('converts package', (done) => {
+        const writeStream = temp.createWriteStream();
+        
         package2zip(fs.createReadStream('test/fixture/package.tgz'))
-            .pipe(fs.createWriteStream('test/fixture/package.zip'))
-            .on('finish', done);
+            .pipe(writeStream)
+            .on('finish', () => {
+                const zip = new DecompressZip(writeStream.path);
+                
+                zip.on('error', done);
+                
+                zip.on('list', (files => {
+                    expect(files).to.deep.equal(['package.json','dist/package2zip.js']);
+                    done();
+                }))
+                
+                zip.list();
+            });
     });
     
     it('converts package with dist folder', (done) => {
+        const writeStream = temp.createWriteStream();
+        
         package2zip(fs.createReadStream('test/fixture/package.tgz'), true)
-            .pipe(fs.createWriteStream('test/fixture/dist.zip'))
-            .on('finish', done);
+            .pipe(writeStream)
+            .on('finish', () => {
+                const zip = new DecompressZip(writeStream.path);
+                
+                zip.on('error', done);
+                
+                zip.on('list', (files => {
+                    expect(files).to.deep.equal(['package2zip.js']);
+                    done();
+                }))
+                
+                zip.list();
+            });
     });
     
     
