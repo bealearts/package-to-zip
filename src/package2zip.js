d5858e193128c429ed40b0065622b791580c9222
davidbeale <david.beale@bealearts.co.uk>
2017-02-09 08:15:53 +0000

    Working with dist option

--- src/package2zip.js
+++ src/package2zip.js
@@ -1,28 +1,12 @@
 
 import tar2zip from 'tar-to-zip';
-import tarStream from 'tar-stream';
-import gunzipMaybe from 'gunzip-maybe';
-import { Readable } from 'stream';
-import fs from 'fs';
 
-export default function package2zip(pack) {
-    return new Promise((resolve, reject) => {
-        checkForDist(pack, (error, hasDist) => {
-            if (error) {
-                return reject(error);
-            }
-            
-            const stream = tar2zip(pack, {
-                filter: hasDist ? distFilter : packageFilter,
-                map: hasDist ? distMap : packageMap
-            })
-            .on('error', reject)
-            .getStream();
-            
-            resolve(stream);
-        });
-        
-    });
+export default function package2zip(packStream, onlyDist=false) {
+    return tar2zip(packStream, {
+        filter: onlyDist ? distFilter : packageFilter,
+        map: onlyDist ? distMap : packageMap
+    })
+        .getStream();
 }
 
 
@@ -45,34 +29,4 @@ function packageMap({ name }) {
     return {
         name: name.substr(8)
     };
-}
-
-
-function checkForDist(pack, callback) {
-    const tar = tarStream.extract();
-    
-    tar.on('entry', (header, stream, next) => {
-        if (header.name.substr(0, 13) === 'package/dist/') {
-            return callback(null, true);
-        }
-        
-        return next();
-    });
-    
-    getFile(pack)
-        .pipe(gunzipMaybe())
-            .pipe(tar)
-                .on('finish', () => callback(null, false))
-                .on('error', (error) => callback(error));
-    
-    return true;
-}
-
-
-function getFile(file) {
-    if (file instanceof Readable) {
-        return file;
-    }
-    
-    return fs.createReadStream(file);
 }
--- test/package2zip.spec.js
+++ test/package2zip.spec.js
@@ -5,11 +5,16 @@ import fs from 'fs';
 describe('package2zip', () => {
     
     it('converts package', (done) => {
-        package2zip('test/fixture/package.tgz')
-            .then((stream) => {
-                stream.pipe(fs.createWriteStream('test/fixture/package.zip'))
-                    .on('finish', done);
-            });
+        package2zip(fs.createReadStream('test/fixture/package.tgz'))
+            .pipe(fs.createWriteStream('test/fixture/package.zip'))
+            .on('finish', done);
     });
+    
+    it('converts package with dist folder', (done) => {
+        package2zip(fs.createReadStream('test/fixture/package.tgz'), true)
+            .pipe(fs.createWriteStream('test/fixture/dist.zip'))
+            .on('finish', done);
+    });
+    
     
 });
